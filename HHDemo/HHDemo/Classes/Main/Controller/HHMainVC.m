9bb74e7094d7ba1ee09e83f27c9cec946e1a82a0
huanghuacai <18811108252@163.com>
2016-11-09 22:57:06 +0800

    初始化项目

---
+++ HHDemo/HHDemo/Classes/Main/Controller/HHMainVC.m
@@ -0,0 +1,39 @@
+//
+//  HHMainVC.m
+//  HHDemo
+//
+//  Created by 蔡万鸿 on 2016/11/9.
+//  Copyright © 2016年 黄花菜. All rights reserved.
+//
+
+#import "HHMainVC.h"
+
+@interface HHMainVC ()
+
+@end
+
+@implementation HHMainVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.title = @"笔记列表";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
