6ec409c66d829d73bc429bd08eafeab9a3280079
huanghuacai <18811108252@163.com>
2017-03-15 23:55:51 +0800

    字符串分类

---
+++ HHDemo/HHDemo/Classes/Category/NSString+HHExtension.h
@@ -0,0 +1,13 @@
+//
+//  NSString+HHExtension.h
+//  HHDemo
+//
+//  Created by 蔡万鸿 on 2017/3/15.
+//  Copyright © 2017年 黄花菜. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HHExtension)
+
+@end
---
+++ HHDemo/HHDemo/Classes/Category/NSString+HHExtension.m
@@ -0,0 +1,13 @@
+//
+//  NSString+HHExtension.m
+//  HHDemo
+//
+//  Created by 蔡万鸿 on 2017/3/15.
+//  Copyright © 2017年 黄花菜. All rights reserved.
+//
+
+#import "NSString+HHExtension.h"
+
+@implementation NSString (HHExtension)
+
+@end
