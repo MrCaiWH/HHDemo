099b66df5f50a662fc7ccb4b3a14ab8339118220
蔡万鸿 <wanhong.cai@zerouav.com>
2017-04-25 10:59:12 +0800

    PromiseKit

---
+++ HHDemo/Pods/Target Support Files/PromiseKit/PromiseKit-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "CALayer+PromiseKit.h"
+#import "NSNotificationCenter+PromiseKit.h"
+#import "NSURLConnection+PromiseKit.h"
+#import "Promise+Pause.h"
+#import "PromiseKit.h"
+#import "Promise.h"
+#import "fwd.h"
+#import "UIActionSheet+PromiseKit.h"
+#import "UIAlertView+PromiseKit.h"
+#import "UIView+PromiseKit.h"
+#import "UIViewController+PromiseKit.h"
+#import "Promise+Until.h"
+#import "Promise+When.h"
+
+FOUNDATION_EXPORT double PromiseKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
+
