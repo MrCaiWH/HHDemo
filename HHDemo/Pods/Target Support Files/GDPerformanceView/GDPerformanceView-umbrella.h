3f54893c2481c8f8bace51dd6a93f3d40f130aa6
蔡万鸿 <wanhong.cai@zerouav.com>
2017-08-04 15:55:38 +0800

    更新pods

---
+++ HHDemo/Pods/Target Support Files/GDPerformanceView/GDPerformanceView-umbrella.h
@@ -0,0 +1,13 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "GDMarginLabel.h"
+#import "GDPerformanceMonitor.h"
+#import "GDPerformanceMonitorProtocol.h"
+#import "GDPerformanceView.h"
+#import "GDWindowViewController.h"
+
+FOUNDATION_EXPORT double GDPerformanceViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char GDPerformanceViewVersionString[];
+
---
+++ HHDemo/Pods/Target Support Files/KMCGeigerCounter/KMCGeigerCounter-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "KMCGeigerCounter.h"
+
+FOUNDATION_EXPORT double KMCGeigerCounterVersionNumber;
+FOUNDATION_EXPORT const unsigned char KMCGeigerCounterVersionString[];
+
