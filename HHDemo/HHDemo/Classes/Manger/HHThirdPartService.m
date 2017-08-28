a25af8dee985c9dd8861985419e9ee43f1962788
蔡万鸿 <wanhong.cai@zerouav.com>
2017-08-28 10:43:51 +0800

    集成bugly

--- HHDemo/HHDemo/Classes/Manger/HHThirdPartService.m
+++ HHDemo/HHDemo/Classes/Manger/HHThirdPartService.m
@@ -15,7 +15,17 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         //异常检测，Crash收集
-        [Bugly startWithAppId:@"005c4ad1be"];
+        BuglyConfig *config = [[BuglyConfig alloc] init];
+        //非正常退出事件
+        config.unexpectedTerminatingDetectionEnable = YES;
+        //卡顿监控
+        config.blockMonitorEnable = YES;
+        //开启SDK日志
+        config.debugMode = YES;
+        //进程内还原符号
+        config.symbolicateInProcessEnable = YES;
+        // 自定义config
+        [Bugly startWithAppId:@"BUGLY_APP_ID" config:config];
         
         NSLog(@"第三方服务注册完毕");
     });
